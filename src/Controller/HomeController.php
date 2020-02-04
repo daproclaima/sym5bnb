524523e67fb49db98181604948b3a33f8631f13e
DaProclaima <sn.itpro@gmail.com>
2020-02-04 14:32:41 +0100

    initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+        return new Response("
+            <html>
+                <head>
+                    <title> Mon application Smfony 5 Rbnb</title>    
+                </head>
+                <body>
+                    <h1>Bonjour à tous</h1>
+                    <p> Nous y voilà!</p>
+        ");
+    }
+}
+
+?>
