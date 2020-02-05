91c994ff331428038118c234e6a18d1ef7736599
DaProclaima <sn.itpro@gmail.com>
2020-02-05 18:14:13 +0100

    Revert "generate 30 fixtures with faker"
    This reverts commit c01e144b7e5067cc2f5798efa49b9d45a682f8f4.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,38 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('Fr-fr');
-        for($i = 1; $i <=30; $i++ ){
+        $ad = new Ad();
 
-            $ad = new Ad();
+        $ad->setTitle("Titre de l'annonce")
+        ->setSlug('titre-de-l-annonce')
+        ->setCoverImage('http://placehold.it/1000x300')
+        ->setIntroduction("Bonjour à tous c'est une introduction")
+        ->setContent("<p>Je suis un contenu riche !</p>")
+        ->setPrice(80)
+        ->setRooms(3);
+        
+        $manager->persist($ad);
 
-            $title = $faker->sentence();
-            $coverImage = $faker->imageUrl(1000, 350);
-            $introduction = $faker->paragraph(2);
-            $content = '<p>'. join('</p><p>', $faker->paragraphs(5)) . '</p>';
-            
-
-            $ad->setTitle($title)
-            ->setSlug("titre-de-l-annonce n.$i")
-            ->setCoverImage($coverImage)
-            ->setIntroduction($introduction)
-            ->setContent($content)
-            ->setPrice(mt_rand(40, 200))
-            ->setRooms(mt_rand(1,5));
-            
-            // save the content 
-            $manager->persist($ad);
-
-        }
-        // execute only once the query with the whole saved content
         $manager->flush();
-
-        // php bin/console doctrine:fixtures:load  [--append]
     }
 }
