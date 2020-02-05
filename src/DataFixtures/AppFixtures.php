2b8f7bc762d977925fb9f59115fd14e626f0c1c5
DaProclaima <sn.itpro@gmail.com>
2020-02-05 18:06:54 +0100

    generate 30 fixtures with faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,17 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('Fr-fr');
         for($i = 1; $i <=30; $i++ ){
 
             $ad = new Ad();
 
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'. join('</p><p>', $faker->paragraph(5)) . '</p>';
+            
+
             $ad->setTitle("Titre de l'annonce n.$i")
-            ->setSlug("titre-de-l-annonce n.$i")
+            ->setSlug('titre-de-l-annonce n.$i')
             ->setCoverImage('http://placehold.it/1000x300')
             ->setIntroduction("Bonjour à tous c'est une introduction")
             ->setContent("<p>Je suis un contenu riche !</p>")
