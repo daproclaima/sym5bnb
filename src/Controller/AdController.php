6bdb6c69699f600bf53e69dcb3e06a1947ee5831
DaProclaima <sn.itpro@gmail.com>
2020-02-06 12:17:59 +0100

    updates:finding one ad with its slug with new url ads/{slug}

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
@@ -14,14 +14,28 @@ class AdController extends AbstractController
      * @Route("/ads", name="ads_index")
      * php bin/console debug:autowiring --all is a core command
      */
-    public function index(AdRepository $repo, SessionInterface $session)
+    public function index(AdRepository $repo)
     {
-        dump($session);
-        // $repo = $this->getDoctrine()->getRepository(Ad::class);
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads
+        ]);
+    }
+
+    /**
+     * Shows one ad
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo) {
+        // Finds the ad matching with the given slug !
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad'=> $ad
         ]);
     }
 }
