3d613310f525d75ddd85cc4c7c0536f382b71f0f
DaProclaima <sn.itpro@gmail.com>
2020-02-06 09:17:48 +0100

    updates(AppFixtures.php): uses slugify

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -12,11 +13,13 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('Fr-fr');
+        $slugify = new Slugify();
         for($i = 1; $i <=30; $i++ ){
 
             $ad = new Ad();
 
             $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
             $coverImage = $faker->imageUrl(1000, 350);
             $introduction = $faker->paragraph(2);
             $content = '<p>'. join('</p><p>', $faker->paragraphs(5)) . '</p>';
