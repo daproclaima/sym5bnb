25baafa12cc169b9b8af69c8e9c8e146ca35800d
DaProclaima <sn.itpro@gmail.com>
2020-02-06 15:55:42 +0100

    feat(AdType):creating a form can be a lot of work. Not working. Leveling up the whole form both class and its twig view

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,18 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
+            ->add('title', TextType::class, [
+                'label'=> 'Titre',
+                'attr'=> [
+                    'placeholder'=> 'Tapez un super titre pour votre annonce'
+                ]
+            ])
+            ->add('slug', TextType::class,[
+                'label' => 'Chaine URL',
+                'attr' => [
+                    'placeholder' => "lien web de l'annonce (automatique)"
+                ]
+            ])
             ->add('price')
             ->add('introduction')
             ->add('content')
