0851d9fc6a13eeea0d95298ff307c721226f19cd
DaProclaima <sn.itpro@gmail.com>
2020-02-06 15:26:56 +0100

    feat(AdController::create()): creates form automatically linked to attrributes type, secured. Shows the form but with no style

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -31,7 +31,21 @@ class AdController extends AbstractController
      * @return Response
      */
     public function create(){
-        return $this->render('ad/new.html.twig');
+
+        $ad = new Ad();
+
+        $form = $this->createFormBuilder($ad)
+            ->add('title')
+            ->add('introduction')
+            ->add('content')
+            ->add('rooms')
+            ->add('price')
+            ->add('coverImage')
+            ->getForm();
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
