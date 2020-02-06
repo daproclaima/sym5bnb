532e4d78827bfb0df1a75a67028a9596cc68f12b
DaProclaima <sn.itpro@gmail.com>
2020-02-06 12:06:43 +0100

    updates:comment about php bin/console debug:autowiring --all as must be known command.

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -12,6 +12,7 @@ class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
+     * php bin/console debug:autowiring --all is a core command
      */
     public function index(AdRepository $repo, SessionInterface $session)
     {
