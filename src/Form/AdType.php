db39dee890520a41d5a30bc33b09b89d05f8c7db
DaProclaima <sn.itpro@gmail.com>
2020-02-06 16:15:09 +0100

    feat(AdType, ads/new.html.twig):Form generation clear and the form is shown

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -4,32 +4,44 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdType extends AbstractType
 {
+
+    /**
+     * Gives basic conf of form field
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder) {
+        return [
+            'label'=> $label,
+            'attr'=> [
+                'placeholder'=> $placeholder
+            ]
+        ];
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class, [
-                'label'=> 'Titre',
-                'attr'=> [
-                    'placeholder'=> 'Tapez un super titre pour votre annonce'
-                ]
-            ])
-            ->add('slug', TextType::class,[
-                'label' => 'Chaine URL',
-                'attr' => [
-                    'placeholder' => "lien web de l'annonce (automatique)"
-                ]
-            ])
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
+            ->add('title', TextType::class, $this->getConfiguration("Titre","Tapez un super titre pour votre annonce"))
+            ->add('slug', TextType::class, $this->getConfiguration("Chaine URL", "Lien web de l'annonce (automatique)"))
+            ->add('coverImage', UrlType::class,$this->getConfiguration("URL de l'image principale", "Donnez l'adresse URL d'une image donnant vraiment envie de venir chez vous !"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Donnez la description globale de l'annonce"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description détaillée", "Tapez une description donnant vraiment envie de venir chez vous !"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Donnez le nombre de chambres disponibles"))
+            ->add('price', MoneyType::class,$this->getConfiguration("Prix par nuit", "Indiquez le prix désiré pour une nuit"))
         ;
     }
 
