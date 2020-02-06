8870c658a360d74d6f21d70004b2cde160ab651e
DaProclaima <sn.itpro@gmail.com>
2020-02-06 12:05:28 +0100

    updates:  AdController to use dependency injection.

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
@@ -11,10 +13,10 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo, SessionInterface $session)
     {
-        $repo = $this->getDoctrine()
-            ->getRepository(Ad::class);
+        dump($session);
+        // $repo = $this->getDoctrine()->getRepository(Ad::class);
         $ads = $repo->findAll();
 
         return $this->render('ad/index.html.twig', [
