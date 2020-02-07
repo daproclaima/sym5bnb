9a00760b84e7b43b501f44b9849a6e28f3fb07d4
DaProclaima <sn.itpro@gmail.com>
2020-02-07 17:45:20 +0100

    feat: login error message working

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,14 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
+     * Shows and manage  user connection form
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        // dump($error);
+        return $this->render('account/login.html.twig', [
+            "hasError" => $error !== null,
+            "username" => $username
+        ]);
+    }
+
+
+    /**
+     * Allows to log out user
+     *
+     * 
+     * @Route("/logout",name="account_logout")
+     * @return void
+     */
+    public function logout() {
+        // .. nothing
     }
 }
