7d33c8ced6c1b0f1bb2b4d064179981a10baf0b5
DaProclaima <sn.itpro@gmail.com>
2020-02-20 15:06:42 +0100

    feat(AccountController.php, profile.html.twig): allows profile modification

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 
 use Doctrine\ORM\EntityManagerInterface;
@@ -76,6 +77,34 @@ class AccountController extends AbstractController
         return $this->render('account/registration.html.twig', [
             'form' => $form->createView()
         ]);
+    }
 
+    /**
+     * Shows and deal with the user profile modification form
+     *
+     * @Route("/account/profile", name="account_profile")
+     * @return Response
+     */
+    public function profile(Request $request, EntityManagerInterface $manager) {
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            // normaly unecessary since the user already exists in db
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les données du profil ont été enregistré avec succès"
+            );
+        }
+
+        return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
