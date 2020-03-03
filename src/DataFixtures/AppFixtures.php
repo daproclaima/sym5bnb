c6c5136d5a0c028445b329c6d90e34d7cc017d00
DaProclaima <sn.itpro@gmail.com>
2020-03-03 17:47:14 +0100

    test(AppFixtures.php): create fixtures for bookings

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Booking;
 use App\Entity\Image;
 use App\Entity\Role;
 use App\Entity\User;
@@ -98,6 +99,27 @@ class AppFixtures extends Fixture
                 $manager->persist($image);
             }
 
+            // handles bookings
+            for ($j = 1; $j < mt_rand(0,10); $j++) {
+                $booking = new Booking();
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+
+                $duration = mt_rand(3, 10);
+                $endDate = (clone $startDate)->modify("+$duration days");
+                $amount = $ad->getPrice() * $duration;
+
+                $booker = $users[mt_rand(0, count($users) -1)];
+                
+                $booking->setBooker($booker)
+                ->setAd($ad)
+                ->setStartDate($startDate)
+                ->setEndDate($endDate)
+                ->setCreatedAt($createdAt)
+                ->setAmount($amount);
+
+                $manager->persist($booking);
+            }
         }
         // execute only once the query with the whole saved content
         $manager->flush();
