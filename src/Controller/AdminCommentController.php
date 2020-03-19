812adc6cba5d3842d079a25943738df520d1f0c7
daproclaima <sn.itpro@gmail.com>
2020-03-19 17:11:50 +0100

    feat: adds comment s content edition to admin

---
+++ src/Controller/AdminCommentController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\AdminCommentType;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminCommentController extends AbstractController
+{
+    /**
+     * @Route("/admin/comments", name="admin_comments_index")
+     */
+    public function index(CommentRepository $repo)
+    {
+        return $this->render('admin/comment/index.html.twig', [
+            'comments' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Grants comment edition
+     * @Route("/admin/comments/{id}/edit", name="admin_comment_edit")
+     * @param Comment $comment
+     * @return Response
+     */
+    public function edit (Comment $comment,  Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(AdminCommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$comment->getId() }</strong> a bien été enregistrée !"
+            );
+
+        }
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+}
