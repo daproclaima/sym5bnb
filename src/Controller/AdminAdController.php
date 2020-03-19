a188bdf6fee05b5b99731cc3168c8ebd42900f0d
daproclaima <sn.itpro@gmail.com>
2020-03-19 13:45:12 +0100

    feat: adds pictures preview and deletion in ad edition

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminAdController extends AbstractController
 {
@@ -17,6 +21,32 @@ class AdminAdController extends AbstractController
 
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Undocumented function
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit (Ad $ad,  Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(AdType::class, $ad);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
+        }
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
