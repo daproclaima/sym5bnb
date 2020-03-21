83d7c13ebbf48fec00bf61b55a0bfa2f069971a0
daproclaima <sn.itpro@gmail.com>
2020-03-21 23:01:47 +0100

    feat: adds StatsService

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Stats;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,46 +12,14 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/admin/", name="admin_dashboard")
      */
-    public function index(EntityManagerInterface $manager)
+    public function index(EntityManagerInterface $manager, Stats $statsService)
     {
-
-        $users = $manager->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
-        // dump($users);
-        
-        $ads = $manager->createQuery('SELECT COUNT(a) FROM App\Entity\Ad a')->getSingleScalarResult();
-        // dump($ads);
-
-        $bookings = $manager->createQuery('SELECT COUNT(b) FROM App\Entity\Booking b')->getSingleScalarResult();
-        // dump($bookings);
-
-        $comments = $manager->createQuery('SELECT COUNT(c) FROM App\Entity\Comment c')->getSingleScalarResult();
-        // dump($comments);
-
-
-        $bestAds = $manager->createQuery(
-            'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
-            FROM App\Entity\Comment c
-            JOIN c.ad a
-            JOIN a.author u
-            GROUP BY a
-            ORDER BY note DESC'
-        )->setMaxResults(5)
-        ->getResult();
-        // dump($bestAds);
-
-        $worstAds = $manager->createQuery(
-            'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
-            FROM App\Entity\Comment c
-            JOIN c.ad a
-            JOIN a.author u
-            GROUP BY a
-            ORDER BY note ASC'
-        )->setMaxResults(5)
-        ->getResult();
-        // dump($worstAds);
+        $stats = $statsService->getStats();
+        $bestAds = $statsService->getAdsStats('DESC');
+        $worstAds = $statsService->getAdsStats('ASC');
 
         return $this->render('admin/dashboard/index.html.twig', [
-            'stats' => compact('users', 'ads', 'bookings', 'comments'),
+            'stats' => $stats,
             'bestAds' => $bestAds,
             'worstAds' => $worstAds
         ]);
