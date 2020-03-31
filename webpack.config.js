f6ead1644e38ef10c47d0ca1aceb12bc98e5643d
daproclaima <sn.itpro@gmail.com>
2020-03-31 12:30:28 +0200

    adds all

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,19 @@ Encore
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
+    .copyFiles({
+                 from: './assets/images',
+        
+                 // optional target path, relative to the output dir
+                to: 'images/[path][name].[ext]',
+        
+                 // if versioning is enabled, add the file hash too
+                 to: 'images/[path][name].[hash:8].[ext]',
+        
+                 // only copy files matching this pattern
+                 pattern: /\.(png|jpg|jpeg)$/
+             })
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
