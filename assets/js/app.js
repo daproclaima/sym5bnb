78c4ddf196e179a5392820b78d6e138dbea4c144
daproclaima <sn.itpro@gmail.com>
2020-03-31 12:30:28 +0200

    adds all

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,6 @@ require('./datepicker.common.js')
 require('./datepicker.min.js')
 require('./fontawesome.js')
 require('@chenfengyuan/datepicker/i18n/datepicker.fr-FR')
+const faviconPath = require('../images/favicon_symbol.png');
+
+let html = `<link rel="favicon" href="${faviconPath}" alt="favicon sym5BnB">`
